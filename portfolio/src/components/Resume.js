3306ca7e19e8dbe3561aded5609bb86a1a16862e
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-07-13 19:39:26 +1000

    Resume logic

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -8,6 +8,25 @@ function Header() {
       </header>
     );
   }
+function Resume({ currentSection }) {
+    const proficiencies = ['Proficiency 1', 'Proficiency 2', 'Proficiency 3'];
+  
+    return (
+      <section className={currentSection === 'Resume' ? 'selected' : ''}>
+        <h2>Resume</h2>
+        <a href="path/to/resume.pdf" download>
+          Download Resume
+        </a>
+  
+        <h3>Proficiencies</h3>
+        <ul>
+          {proficiencies.map((proficiency, index) => (
+            <li key={index}>{proficiency}</li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
   
 
 export default Header;
